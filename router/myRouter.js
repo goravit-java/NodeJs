1b3ad38873b01d52b979352bf7668bd9d8486fc2
Your Name <you@example.com>
2025-06-24 15:32:54 +0700

    u1

--- router/myRouter.js
+++ router/myRouter.js
@@ -2,10 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/',(req,res)=>{
-    const name = "goravit"
-    const age = 15
+    const product =[{name:"apple",price:100,image:"images/products/product1.png"}
+        ,{name:"banana",price:50,image:"images/products/product2.png"},
+        {name:"orange",price:80,image:"images/products/product3.png"}]
     const address = "<h1>helloWorld</h1>"
-    res.render('index.ejs',{data:name,age:age,address:address})
+    res.render('index.ejs',{address:address,product:product})
+})
+
+router.get('/addForm',(req,res)=>{
+    res.render('form.ejs')
+})
+router.get('/manage',(req,res)=>{
+    res.render('manage.ejs')
 })
 
 
